5a105652fd989b7495f516d4c37a567c7855ccc2
Maul <mprambadi@gmail.com>
2019-02-15 19:53:59 +0700

    Updates

---
+++ precache-manifest.a086a0417151c40f01a3a57311797992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a288a772e0eb3f80972",
+    "url": "/bot-dashboard/static/js/runtime~main.9a288a77.js"
+  },
+  {
+    "revision": "99ff6bb5caab9fb14d0e",
+    "url": "/bot-dashboard/static/js/main.99ff6bb5.chunk.js"
+  },
+  {
+    "revision": "3cde57193486fbb087d2",
+    "url": "/bot-dashboard/static/js/1.3cde5719.chunk.js"
+  },
+  {
+    "revision": "99ff6bb5caab9fb14d0e",
+    "url": "/bot-dashboard/static/css/main.fac706c2.chunk.css"
+  },
+  {
+    "revision": "3cde57193486fbb087d2",
+    "url": "/bot-dashboard/static/css/1.dd47c46b.chunk.css"
+  },
+  {
+    "revision": "4ff4f3011da00dd711da3f31e708b19d",
+    "url": "/bot-dashboard/index.html"
+  }
+];
