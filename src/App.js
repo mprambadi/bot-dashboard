6e370df5f26fdacf15641aeed1209404b56f463a
Maul <mprambadi@gmail.com>
2019-02-24 00:13:46 +0700

    add ma7/25 indicator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
 import { View, Text, Picker, TextInput, TouchableOpacity } from "react-native";
-import { List, AutoSizer } from "react-virtualized";
+import {
+	List,
+	AutoSizer,
+	CellMeasurer,
+	CellMeasurerCache
+} from "react-virtualized";
 import Axios from "axios";
 
 const api = Axios.create({
-	baseURL: "https://telegrafme.herokuapp.com/indicator/"
+	baseURL: "https://telegrafme.herokuapp.com/indicator"
 });
 
 const service = {
 	fetchOhlcv: () => api.get("/binance")
 };
+
 class App extends React.PureComponent {
 	constructor() {
 		super();
@@ -18,13 +24,20 @@ class App extends React.PureComponent {
 			"wss://stream.binance.com:9443/ws/!miniTicker@arr@3000ms"
 		);
 	}
+
 	state = {
 		market: [],
 		chunk: [],
 		base: "BTC",
 		search: "",
-		sort: false
+		sort: false,
+		favorite: [],
+		page: "dashboard"
 	};
+
+	_cache = new CellMeasurerCache({ defaultHeight: 50, fixedWidth: true });
+	_mostRecentWidth = 0;
+
 	componentDidMount() {
 		this.fetchAllTickers();
 		this.interval = setInterval(async () => {
@@ -33,11 +46,13 @@ class App extends React.PureComponent {
 				market: state.market.map(item => {
 					return {
 						bg: item.bg,
+						toggle: item.toggle,
 						...data.find(res => res.id === item.id)
 					};
 				})
 			}));
 		}, 60000);
+		this.getFavorite();
 	}
 
 	componentWillUnmount() {
@@ -51,7 +66,8 @@ class App extends React.PureComponent {
 		const { data } = await service.fetchOhlcv();
 		const addExtraData = data.map(item => ({
 			...item,
-			bg: "white"
+			bg: "white",
+			toggle: false
 		}));
 		this.setState({ market: addExtraData }, () => {
 			this.getSocket();
@@ -93,91 +109,6 @@ class App extends React.PureComponent {
 			}));
 		};
 	}
-
-	renderIndicator = ({ index, key, style }) => {
-		const filterBase = this.state.market.filter(
-			item => item.base === this.state.base
-		);
-
-		const filteredData = filterBase.filter(
-			item =>
-				[item.id, item.fib.s1.percentage]
-					.join("")
-					.indexOf(this.state.search.toUpperCase()) !== -1
-		);
-
-		const {
-			id,
-			ticker,
-			rsi: { daily, fourly },
-			market: { precision },
-			bg,
-			fib: { s3, s2, s1, p, r3, r2, r1 },
-			ma: { maFourly }
-		} = filteredData[index];
-
-		return (
-			<View
-				style={[
-					{
-						flexDirection: "row",
-						justifyContent: "space-between",
-						flex: 1,
-						borderBottomColor: "black",
-						borderWidth: 1
-					},
-					style
-				]}
-				key={key}
-			>
-				<TextCenter text={id} />
-				<TextCenter
-					text={fixedNumberBy(ticker.last, precision.price)}
-					backgroundColor={bg}
-					bold
-				/>
-				<TextCenter
-					text={Number(ticker.percentage).toFixed(2)}
-					percentage
-					backgroundColor={
-						ticker.percentage > 0
-							? "#42f474"
-							: ticker.percentage < 0
-							? "#f44141"
-							: "white"
-					}
-				/>
-
-				<TextCenter
-					text={fixedNumberBy(fourly.lastRSI, 2)}
-					backgroundColor={fourly.lastRSI < 25 ? "#42f474" : "white"}
-				/>
-				<TextCenter
-					text={fixedNumberBy(daily.lastRSI, 2)}
-					backgroundColor={daily.lastRSI < 25 ? "#42f474" : "white"}
-				/>
-				<TextCenter
-					text={s1.percentage}
-					percentage
-					backgroundColor={s1.percentage < 1 ? "#42f474" : "white"}
-				/>
-				<TextCenter
-					text={maFourly.percentage}
-					percentage
-					backgroundColor={ticker.last > maFourly.price ? "#42f474" : "#f44141"}
-				/>
-
-				<TextCenter text={fixedNumberBy(r3.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(r2.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(r1.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(p.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(s1.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(s2.price, precision.price)} />
-				<TextCenter text={fixedNumberBy(s3.price, precision.price)} />
-			</View>
-		);
-	};
-
 	orderBy = () => {
 		this.setState(state => {
 			return {
@@ -193,17 +124,194 @@ class App extends React.PureComponent {
 		});
 	};
 
-	render() {
-		const filterBase = this.state.market.filter(
-			item => item.base === this.state.base
-		);
+	saveFavorite = () => {
+		localStorage.setItem("favorite", JSON.stringify(this.state.favorite));
+	};
+
+	getFavorite = () => {
+		const data = JSON.parse(localStorage.getItem("favorite"));
+		this.setState({ favorite: data ? data : [] });
+	};
+
+	renderIndicator = ({ index, key, style, parent }) => {
+		const { market, favorite, page, search, base } = this.state;
+
+		const filterBase = market.filter(item => item.base === base);
 
 		const filteredData = filterBase.filter(
 			item =>
 				[item.id, item.fib.s1.percentage]
 					.join("")
-					.indexOf(this.state.search.toUpperCase()) !== -1
+					.indexOf(search.toUpperCase()) !== -1
 		);
+
+		const favorites =
+			page === "dashboard"
+				? filteredData
+				: favorite.map(item => market.find(filter => filter.id === item));
+
+		const {
+			id,
+			ticker,
+			rsi: { daily, fourly },
+			market: { precision },
+			bg,
+			toggle,
+			fib: { s3, s2, s1, p, r3, r2, r1 },
+			ma: { maFourly : {ninety, seven, twentyFive, twentyFiveSeven} }
+		} = favorites[index];
+
+		return (
+			<CellMeasurer
+				cache={this._cache}
+				columnIndex={0}
+				key={key}
+				parent={parent}
+				rowIndex={index}
+				width={this._mostRecentWidth}
+			>
+				<View
+					style={[
+						{
+							borderBottomColor: "black",
+							borderWidth: 1,
+							minHeight: 60
+						},
+						style
+					]}
+					key={key}
+				>
+					<View
+						style={{
+							flexDirection: "row",
+							justifyContent: "space-between",
+							flex: 1
+						}}
+					>
+						<TextCenter
+							text={id}
+							onPress={() =>
+								this.setState(
+									state => ({
+										favorite:
+											page === "dashboard"
+												? [...state.favorite, id]
+												: state.favorite.filter(item => item !== id)
+									}),
+									() => {
+										this.saveFavorite();
+									}
+								)
+							}
+						/>
+
+						<TextCenter
+							text={fixedNumberBy(ticker.last, precision.price)}
+							backgroundColor={bg}
+							bold
+							onPress={() =>
+								this.setState(state => ({
+									market: state.market.map(item =>
+										item.id === id
+											? { ...item, toggle: !item.toggle }
+											: { ...item }
+									)
+								}))
+							}
+						/>
+
+						<TextCenter
+							text={Number(ticker.percentage).toFixed(2)}
+							percentage
+							backgroundColor={
+								ticker.percentage > 0
+									? "#42f474"
+									: ticker.percentage < 0
+									? "#f44141"
+									: "white"
+							}
+						/>
+
+						<TextCenter
+							text={fixedNumberBy(fourly.lastRSI, 2)}
+							backgroundColor={fourly.lastRSI < 25 ? "#42f474" : "white"}
+						/>
+						<TextCenter
+							text={fixedNumberBy(daily.lastRSI, 2)}
+							backgroundColor={daily.lastRSI < 25 ? "#42f474" : "white"}
+						/>
+						<TextCenter
+							text={s1.percentage}
+							percentage
+							backgroundColor={s1.percentage < 1 ? "#42f474" : "white"}
+						/>
+						<TextCenter
+							text={ninety.percentage}
+							percentage
+							backgroundColor={
+								ticker.last > ninety.price ? "#42f474" : "#f44141"
+							}
+						/>
+						<TextCenter
+							text={twentyFiveSeven.percentage}
+							percentage
+							backgroundColor={
+								seven.price > twentyFive.price ? "#42f474" : "#f44141"
+							}
+						/>
+
+						<TextCenter text={fixedNumberBy(r3.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(r2.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(r1.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(p.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(s1.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(s2.price, precision.price)} />
+						<TextCenter text={fixedNumberBy(s3.price, precision.price)} />
+					</View>
+
+					{toggle && (
+						<View style={{ flex: 1, minHeight: 40, backgroundColor: "blue" }}>
+							<View style={{ flexDirection: "row", flex:1, justifyContent:'space-around' }}>
+								<Text>Ok 1</Text>
+								<Text>Ok 2</Text>
+								<Text>Ok 3</Text>
+								<Text>Ok 4</Text>
+								<Text>Ok 5</Text>
+								<Text>Ok 6</Text>
+								<Text>Ok 7</Text>
+							</View>
+							<View style={{ flexDirection: "row", flex:1, justifyContent:'space-around' }}>
+								<Text>Ok 1</Text>
+								<Text>Ok 2</Text>
+								<Text>Ok 3</Text>
+								<Text>Ok 4</Text>
+								<Text>Ok 5</Text>
+								<Text>Ok 6</Text>
+								<Text>Ok 7</Text>
+							</View>
+						</View>
+					)}
+				</View>
+			</CellMeasurer>
+		);
+	};
+
+	render() {
+		const { market, favorite, page, search, base } = this.state;
+
+		const filterBase = market.filter(item => item.base === base);
+
+		const filteredData = filterBase.filter(
+			item =>
+				[item.id, item.fib.s1.percentage]
+					.join("")
+					.indexOf(search.toUpperCase()) !== -1
+		);
+
+		const favorites =
+			page === "dashboard"
+				? filteredData
+				: favorite.map(item => market.find(filter => filter.id === item));
 
 		return (
 			<View>
@@ -218,54 +326,67 @@ class App extends React.PureComponent {
 						paddingTop: 10,
 						zIndex: 1,
 						flexDirection: "row",
-						alignItems: "center"
+						alignItems: "center",
+						justifyContent: "space-around"
 					}}
 				>
-					<Text>Base Pair : </Text>
-					<Picker
-						selectedValue={this.state.base}
-						style={{
-							height: 30,
-							width: 100
-						}}
-						onValueChange={itemValue => this.setState({ base: itemValue })}
-					>
-						<Picker.Item label="BTC" value="BTC" />
-						<Picker.Item label="USDT" value="USDT" />
-						<Picker.Item label="TUSD" value="TUSD" />
-						<Picker.Item label="PAX" value="PAX" />
-						<Picker.Item label="ETH" value="ETH" />
-					</Picker>
+					<View style={{ flexDirection: "row" }}>
+						<Text>Base Pair : </Text>
+						<Picker
+							selectedValue={base}
+							style={{
+								height: 30,
+								width: 100
+							}}
+							onValueChange={itemValue => this.setState({ base: itemValue })}
+						>
+							<Picker.Item label="BTC" value="BTC" />
+							<Picker.Item label="USDT" value="USDT" />
+							<Picker.Item label="TUSD" value="TUSD" />
+							<Picker.Item label="PAX" value="PAX" />
+							<Picker.Item label="ETH" value="ETH" />
+						</Picker>
+					</View>
+					<View style={{ flexDirection: "row", alignItems: "center" }}>
+						<Text>Search :</Text>
+						<TextInput
+							onChange={({ target: { value } }) =>
+								this.setState({ search: value })
+							}
+							style={{
+								borderColor: "black",
+								width: 150,
+								height: 25,
+								borderWidth: 1
+							}}
+						/>
+					</View>
 
-					<View
-						style={{ flexDirection: "row", alignItems: "center", width: "20%" }}
+					<TextCenter
+						text={`Favorite ${favorite.length}`}
+						onPress={() => this.setState({ page: "favorite" })}
 					/>
-					<Text>Search :</Text>
-					<TextInput
-						onChange={({ target: { value } }) =>
-							this.setState({ search: value })
-						}
-						style={{
-							borderColor: "black",
-							width: 150,
-							height: 25,
-							borderWidth: 1
-						}}
+					<TextCenter
+						text="Dashboard"
+						onPress={() => this.setState({ page: "dashboard" })}
 					/>
 				</View>
 				<HeaderIndicator orderBy={this.orderBy} />
 				<AutoSizer>
-					{({ width }) => (
-						<List
-							style={{ marginTop: 40 }}
-							rowCount={filteredData.length}
-							rowRenderer={this.renderIndicator}
-							width={width}
-							height={580}
-							overscanRowCount={10}
-							rowHeight={35}
-						/>
-					)}
+					{({ width }) => {
+						return (
+							<List
+								deferredMeasurementCache={this._cache}
+								style={{ marginTop: 40 }}
+								rowCount={favorites.length}
+								rowRenderer={this.renderIndicator}
+								width={width}
+								height={600}
+								overscanRowCount={1}
+								rowHeight={this._cache.rowHeight}
+							/>
+						);
+					}}
 				</AutoSizer>
 			</View>
 		);
@@ -290,14 +411,9 @@ const HeaderIndicator = ({ orderBy }) => (
 		<TextCenter bold text="RSI 4H" />
 		<TextCenter bold text="RSI 1D" />
 		<TextCenter bold text="S1%" />
-		<TouchableOpacity
-			onPress={() => orderBy()}
-			style={{ alignItems: "center" }}
-		>
-			<TextCenter bold text="MA90%" />
-		</TouchableOpacity>
+		<TextCenter bold text="MA90%" onPress={() => orderBy()} />
+		<TextCenter bold text="(MA7/25)%"/>
 		<TextCenter bold text="R3" />
-
 		<TextCenter bold text="R2" />
 		<TextCenter bold text="R1" />
 		<TextCenter bold text="P" />
@@ -307,19 +423,20 @@ const HeaderIndicator = ({ orderBy }) => (
 	</View>
 );
 
-const TextCenter = ({ text, percentage, bold, backgroundColor }) => (
-	<View
+const TextCenter = ({ text, percentage, bold, backgroundColor, onPress }) => (
+	<TouchableOpacity
 		style={{
 			width: "6.0%",
 			alignItems: "center",
 			backgroundColor,
 			justifyContent: "center"
 		}}
+		onPress={onPress}
 	>
 		<Text style={{ fontWeight: bold ? "bold" : "normal" }}>
 			{text} {percentage ? "%" : ""}
 		</Text>
-	</View>
+	</TouchableOpacity>
 );
 export default App;
 
